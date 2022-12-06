c09ae4419188181045da7791edf48e2b96e1b828
EstefaniConislla <stefani_19_94@hotmail.com>
2022-12-06 01:19:07 -0500

    ms/generating first commit

---
+++ mslibrary/src/main/java/library/mslibrary/MslibraryApplication.java
@@ -0,0 +1,13 @@
+package library.mslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MslibraryApplication.class, args);
+	}
+
+}
